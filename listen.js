6bfa978567c8d8c6fd57663247dfdfb9b4906f2d
abdalla <abdallazwolle@hotmail.com>
2021-01-21 13:48:15 +0000

    mmf

--- listen.js
+++ listen.js
@@ -1,5 +1,4 @@
-const express = require("express")
-const app = require("./app")
+const express = require("express");
+const app = require("./app");
 const { PORT = 9090 } = process.env;
-​
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
