ca44a7e00234fbba15ee3c314d624c0c2cd9dc72
abdalla <abdallazwolle@hotmail.com>
2021-02-09 15:00:48 +0000

    essential routes completed

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
+var cors = require("cors");
 const apiRouter = require("./routers/api-router");
 const {
   handleCustomErrors,
   handlePsqlErrors,
   handleServerErrors,
 } = require("./controllers/error.controller");
+app.use(cors());
 app.use(express.json());
 app.use("/api", apiRouter);
 app.use("/*", (req, res, next) => {
